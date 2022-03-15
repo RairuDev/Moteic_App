8817fde0035245595d022c5571bfdc121454bc75
junichmiyahara <junich0911@gmail.com>
2022-03-16 02:53:11 +0900

    slim-install

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module MoteicApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.generators.template_engine = :slim #templateエンジンをslimに変更
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
