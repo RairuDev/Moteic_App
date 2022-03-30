4254f5a80a7fe6f79fd473f0d1eee28092e4924f
junichmiyahara <junich0911@gmail.com>
2022-03-31 01:16:57 +0900

    sing_up_step2_enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+  enum gender: {man: 0, woman: 1}
+  enum body_shape: {'細め': 0, 'やや細め': 1, '普通': 2, 'ぽっちゃり': 3, '筋肉質': 4}
+  enum age: {'18~24': 0, '25~29': 1, '30~34': 2, '35~39': 3, '40~': 4}
 end
