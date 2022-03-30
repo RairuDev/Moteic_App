d578239634751e5deb13542de5f165a495deb86f
H-hidetaka <sansanhidetaka@gmail.com>
2022-03-30 16:58:05 +0900

    [Modify]修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,10 +13,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new(sign_up_params)
   end
 
-  def step3
-    @user
-  end
-
   # POST /resource
   # def create
   #   super
