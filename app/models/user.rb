7a552d5f3ff4fc15ab725753ac04336bfb4d2c13
junichmiyahara <junich0911@gmail.com>
2022-04-01 14:47:29 +0900

    enum-add

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   enum gender: {man: 0, woman: 1}
   enum body_shape: {'細め': 0, 'やや細め': 1, '普通': 2, 'ぽっちゃり': 3, '筋肉質': 4}
   enum age: {'18~24': 0, '25~29': 1, '30~34': 2, '35~39': 3, '40~': 4}
+  enum job: {'IT-engineer': 0, 'IT-relation': 1, 'Web-relation': 2, 'others': 3}
 end
