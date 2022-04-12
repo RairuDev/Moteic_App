77732f02cc1647df7789e84703f0368df524eaed
junichmiyahara <junich0911@gmail.com>
2022-04-12 19:42:48 +0900

    user-value-keep

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,22 +9,62 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   def step2
+    session[:email] = sign_up_params[:email]
+    session[:password] = sign_up_params[:password]
     @user = User.new(sign_up_params)
+    # render :new if @user.invalid?
   end
 
   def step3
+    #step２で入力した情報を保持
+    session[:name] = sign_up_params[:name]
+    session[:gender] = sign_up_params[:gender]
+    session[:body_shape] = sign_up_params[:body_shape]
+    session[:age] = sign_up_params[:age]
+    
     @user = User.new(sign_up_params)
     render :new and return if params[:back]
+    # render :step2 if @user.invalid?
   end
 
   def confirm
+    #step３で入力した情報を保持
+    session[:job] = sign_up_params[:job]
+    session[:income] = sign_up_params[:income]
+    session[:prefecture_id] = sign_up_params[:prefecture_id]
     @user = User.new(sign_up_params)
+    # render :step3 if @user.invalid?
     render :step2 and return if params[:back]
   end
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(
+      email: session[:email],
+      password: session[:password],
+      name: session[:name],
+      gender: session[:gender],
+      body_shape: session[:body_shape],
+      age: session[:age],
+      income: session[:income],
+      job: session[:job],
+      prefecture_id: session[:prefecture_id]
+    )
+    session["devise.regist_data"] = {user: @user.attributes}
+    # session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    # binding.pry
+    render :step3 and return if params[:back]
+    # binding.pry
+    # if @user.invalid?
+    #   flash.now[:alert] = @user.errors.full_messages
+    #   render :confirm and return
+    # else
+    #   @user.save
+    # end
+  end
+
+  def completion
+    @user = current_user
+  end
 
   # GET /resource/edit
   # def edit
